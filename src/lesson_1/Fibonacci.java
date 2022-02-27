3f03959b692da2fa812cb1b4b5eeca54f09e6c35
Vladislav Markov <v1adm4rkov@ya.ru>
2022-02-27 21:15:09 +0300

    Initial commit

---
+++ src/lesson_1/Fibonacci.java
@@ -0,0 +1,40 @@
+package lesson_1;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+    private long[] fibArray;
+    // уменьшение кэша??
+
+    public Fibonacci() {
+    }
+
+    public long recursion(long value) {
+        if (value <= 0) {
+            return 0;
+        } else if (value == 2) {
+            return 1;
+        } else {
+            return recursion(value - 1) + recursion(value - 2);
+        }
+    }
+
+    public void cycle(int capacityFibArray) {
+        fibArray = new long[capacityFibArray];
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+        for (int i = 2; i < fibArray.length; i++) {
+            long result = fibArray[i - 1] + fibArray[i - 2];
+            fibArray[i] = result;
+        }
+        printFibArray();
+    }
+
+    private void printFibArray() {
+        System.out.println(Arrays.toString(getFibArray()));
+    }
+
+    private long[] getFibArray() {
+        return fibArray;
+    }
+}
---
+++ src/lesson_1/Main.java
@@ -0,0 +1,9 @@
+package lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Fibonacci fib = new Fibonacci();
+        System.out.println(fib.recursion(40));
+        fib.cycle(40);
+    }
+}
